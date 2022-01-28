8684471f6be1bb9339024d993a1d464f8bc038f8
favor <favorxog@gmail.com>
2022-01-29 02:36:56 +0300

    display cutout edges at cost of everything

--- components/Reader.js
+++ components/Reader.js
@@ -13,6 +13,8 @@ import Animated, {
   useDerivedValue,
   withTiming,
 } from 'react-native-reanimated';
+import FastImage from 'react-native-fast-image';
+
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 const { width: screenWidth, height: screenHeight } = Dimensions.get('screen');
 
@@ -37,9 +39,11 @@ const MangaReader = () => {
   const offsetY = useSharedValue(0);
   const focalX = useSharedValue(0);
   const focalY = useSharedValue(0);
+  const focalOffsetX = useSharedValue(0);
+  const focalOffsetY = useSharedValue(0);
+  const offsetOriginX = useSharedValue(0);
+  const offsetOriginY = useSharedValue(0);
   const scale = useSharedValue(1);
-  const prev1 = useSharedValue(1);
-  const prev2 = useSharedValue(1);
   const scaleOffset = useSharedValue(1);
 
   useEffect(() => {
@@ -47,15 +51,13 @@ const MangaReader = () => {
     NavigationBar.setVisibilityAsync('hidden');
     StatusBar.setStatusBarHidden(true);
 
-    fetch('https://api.remanga.org/api/titles/chapters/656/')
+    fetch('https://api.remanga.org/api/titles/chapters/81271/')
       .then((res) => res.json())
       .then((res) => setImages(res.content.pages));
 
-    return () => {
-      NavigationBar.setVisibilityAsync('visible');
-      StatusBar.setStatusBarHidden(false);
-    };
+    return () => {};
   }, []);
+
   const pan = Gesture.Pan()
     .averageTouches(true)
     .onBegin(() => {
@@ -65,83 +67,59 @@ const MangaReader = () => {
       offsetY.value = transitionY.value; // tracks previous location
     })
     .onUpdate((e) => {
-      // checks for content boundaries
-      // console.log(
-      //   translationX / scale.value + offsetX.value,
-      //   maxWidth.value,
-      //   translationY / scale.value + offsetY.value,
-      //   maxHeight.value
-      // );
-      if (e.translationY < 0) {
-        transitionY.value =
-          e.translationY / scale.value + offsetY.value < -maxHeight.value
-            ? -maxHeight.value
-            : e.translationY / scale.value + offsetY.value;
-      } else {
-        transitionY.value =
-          e.translationY / scale.value + offsetY.value > 0
-            ? 0
-            : e.translationY / scale.value + offsetY.value;
+      if (e.numberOfPointers >= 2) {
+        console.log('absolutes:', e.absoluteX, e.absoluteY);
       }
-
-      if (e.translationX < 0) {
-        transitionX.value =
-          e.translationX / scale.value + offsetX.value < -maxWidth.value
-            ? -maxWidth.value
-            : e.translationX / scale.value + offsetX.value;
-      } else {
-        transitionX.value =
-          e.translationX / scale.value + offsetX.value > 0
-            ? 0
-            : e.translationX / scale.value + offsetX.value;
-      }
+      transitionX.value = e.translationX / scale.value + offsetX.value;
+      transitionY.value = e.translationY / scale.value + offsetY.value;
     })
     .onEnd((e) => {
       transitionX.value = withDecay({
         velocity: e.velocityX / scale.value,
         deceleration: 0.999,
-        clamp: [-maxWidth.value, 0],
+        // clamp: [-maxWidth.value, 0],
       }); // 'scroll' animation.
       transitionY.value = withDecay({
         velocity: e.velocityY / scale.value,
         deceleration: 0.9996,
-        clamp: [-maxHeight.value, 0],
+        // clamp: [-maxHeight.value, 0],
       }); // 'scroll' animation.
     });
 
   const pinch = Gesture.Pinch()
     .onStart((e) => {
+      // focalOffsetX.value = focalX.value;
+      // focalOffsetY.value = focalY.value;
+      scaleOffset.value = scale.value;
       focalX.value = withTiming(e.focalX, { duration: 128 });
       focalY.value = withTiming(e.focalY, { duration: 128 });
     })
     .onUpdate((e) => {
       scale.value = e.scale * scaleOffset.value;
-    })
-    .onEnd((e) => {
-      scaleOffset.value = scale.value;
+      focalX.value = e.focalX;
+      focalY.value = e.focalY;
     });
 
-  const gesture = Gesture.Race(pan, pinch, Gesture.Simultaneous(pinch, pan));
-
+  const gesture = Gesture.Simultaneous(pinch, pan);
+  console.log(screenWidth);
   const style = useAnimatedStyle(() => ({
     transform: [
-      { translateX: focalX.value },
-      { translateY: focalY.value },
+      { translateX: -focalX.value },
+      { translateY: -focalY.value },
       { translateX: -viewWidth.value / 2 },
       { translateY: -viewHeight.value / 2 },
       { scale: scale.value },
-      { translateX: -focalX.value },
-      { translateY: -focalY.value },
       { translateX: viewWidth.value / 2 },
       { translateY: viewHeight.value / 2 },
+      { translateX: focalX.value },
+      { translateY: focalY.value },
       { translateX: transitionX.value },
       { translateY: transitionY.value },
     ],
   }));
-
   return (
-    <GestureDetector gesture={gesture}>
-      <View style={{ alignItems: 'center' }}>
+    <View style={{ backgroundColor: 'red' }}>
+      <GestureDetector gesture={gesture}>
         <Animated.View
           ref={viewRef}
           onLayout={(event) => {
@@ -158,14 +136,13 @@ const MangaReader = () => {
                 source={{ uri: item.link }}
                 style={{
                   aspectRatio: item.width / item.height,
-                  width: '100%',
                 }}
               />
             );
           })}
         </Animated.View>
-      </View>
-    </GestureDetector>
+      </GestureDetector>
+    </View>
   );
 };
 export default MangaReader;
